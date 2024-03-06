62284c071e5785a09fea090b5c13a1868250a882
Kurt Guardia <kaguardiagamarra@wideagency.com>
2024-03-06 10:45:55 +0100

    Delete recipe function added

--- functions/index.js
+++ functions/index.js
@@ -46,3 +46,62 @@ exports.onCreateRecipe = functions.firestore
       }
     }
   })
+
+exports.onDeleteRecipe = functions.firestore
+  .document('recipes/{recipeId}')
+  .onDelete(async (snapshot) => {
+    const recipe = snapshot.data()
+    const imageUrl = recipe.imageUrl
+
+    if (imageUrl) {
+      const decodedUrl = decodeURIComponent(imageUrl)
+      const startIndex = decodedUrl.indexOf('/o/') + 3
+      const endIndex = decodedUrl.indexOf('?')
+      const fullFilePath = decodedUrl.substring(
+        startIndex,
+        endIndex,
+      )
+      const file = storageBucket.file(fullFilePath)
+
+      console.log(`Attempting to delete: ${fullFilePath}`)
+
+      try {
+        file.delete()
+        console.log('Successfully deleted image.')
+      } catch (error) {
+        console.log(
+          `Failed to delete file: ${error.message}`,
+        )
+      }
+
+      const countDocRef = firestore
+        .collection('recipeCounts')
+        .doc('all')
+      const countDoc = await countDocRef.get()
+
+      if (countDoc.exists) {
+        countDocRef.update({
+          count: admin.firestore.FieldValue.increment(-1),
+        })
+      } else {
+        countDocRef.set({ count: 0 })
+      }
+
+      const recipe = snapshot.data()
+
+      if (recipe.isPublished) {
+        const countPublishedDocRef = firestore
+          .collection('recipeCounts')
+          .doc('published')
+        const countPublishedDoc =
+          await countPublishedDocRef.get()
+        if (countPublishedDoc.exists) {
+          countPublishedDocRef.update({
+            count: admin.firestore.FieldValue.increment(-1),
+          })
+        } else {
+          countPublishedDocRef.set({ count: 0 })
+        }
+      }
+    }
+  })
