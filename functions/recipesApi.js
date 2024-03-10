6444e8ecc6635cffd9f829f4ed131e2c185e024d
Kurt Guardia <kaguardiagamarra@wideagency.com>
2024-03-10 16:24:14 +0100

    added update function

--- functions/recipesApi.js
+++ functions/recipesApi.js
@@ -62,8 +62,154 @@ app.post('/recipes', async (request, response) => {
   }
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello from firebase functions express API')
+app.get('/recipes', async (request, response) => {
+  const authorizationHeader =
+    request.headers['authorization']
+
+  const queryObject = request.query
+  const category = queryObject['category']
+    ? queryObject['category']
+    : ''
+  const orderByField = queryObject['orderByField']
+    ? queryObject['orderByField']
+    : ''
+  const orderByDirection = queryObject['orderByDirection']
+    ? queryObject['orderByDirection']
+    : 'asc'
+  const pageNumber = queryObject['pageNumber']
+    ? queryObject['pageNumber']
+    : ''
+  const perPage = queryObject['perPage']
+    ? queryObject['perPage']
+    : ''
+
+  let isAuth = false
+  let collectionRef = firestore.collection('recipes')
+
+  try {
+    await Utilities.authorizeUser(authorizationHeader, auth)
+
+    isAuth = true
+  } catch (error) {
+    collectionRef = collectionRef.where(
+      'isPublished',
+      '==',
+      true,
+    )
+  }
+
+  if (category) {
+    collectionRef = collectionRef.where(
+      'category',
+      '==',
+      category,
+    )
+  }
+
+  if (orderByField) {
+    collectionRef = collectionRef.orderBy(
+      orderByField,
+      orderByDirection,
+    )
+  }
+
+  if (perPage) {
+    collectionRef = collectionRef.limit(Number(perPage))
+  }
+
+  if (pageNumber > 0 && perPage) {
+    const pageNumberMultiplier = pageNumber - 1
+    const offset = pageNumberMultiplier * perPage
+    collectionRef = collectionRef.offset(offset)
+  }
+
+  let recipeCount = 0
+  let countDocRef
+
+  if (isAuth) {
+    countDocRef = firestore
+      .collection('recipeCounts')
+      .doc('all')
+  } else {
+    countDocRef = firestore
+      .collection('recipeCounts')
+      .doc('published')
+  }
+
+  const countDoc = await countDocRef.get()
+
+  if (countDoc.exists) {
+    const countDocData = countDoc.data()
+
+    if (countDocData) {
+      recipeCount = countDocData.cound
+    }
+  }
+
+  try {
+    const firestoreResponse = await collectionRef.get()
+    const fetchedRecipes = firestoreResponse.docs.map(
+      (recipe) => {
+        const id = recipe.id
+        const data = recipe.data()
+        data.publishDate = data.publishDate._seconds
+
+        return { ...data, id }
+      },
+    )
+
+    const payload = {
+      recipeCount,
+      documents: fetchedRecipes,
+    }
+
+    response.status(200).send(payload)
+  } catch (error) {
+    response.status(400).send(error.message)
+  }
+})
+
+app.put('/recipes', async (request, response) => {
+  const authorizationHeader =
+    request.headers['authorization']
+
+  if (!authorizationHeader) {
+    response
+      .status(401)
+      .send('Missing Authorization Header')
+    return
+  }
+
+  try {
+    await Utilities.authorizeUser(authorizationHeader, auth)
+  } catch (error) {
+    response.status(400).send(error.message)
+    return
+  }
+
+  const id = request.params.id
+  const newRecipe = request.body
+  const missingFields =
+    Utilities.validateRecipePostPut(newRecipe)
+
+  if (missingFields) {
+    response
+      .status(400)
+      .send(
+        `Recipe is not valid. Missing/incalid fields: ${missingFields}`,
+      )
+    return
+  }
+
+  const recipe = Utilities.sanitizeRecipePostPut(newRecipe)
+
+  try {
+    await firestore.collection("recipes").doc(id).set(recipe)
+
+    response.status(200).send({id})
+  } catch (error) {
+    response.status(400).send(error.message)
+  }
 })
 
 if (process.env.NODE_ENV !== 'production') {
